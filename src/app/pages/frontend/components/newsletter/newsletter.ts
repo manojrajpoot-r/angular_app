45c83af169fc8127f1b785a7633daf12ee083400
manojrajpoot-r <manojrajpoot@08gmail.com>
2026-05-23 17:39:26 +0530

    new letter

--- src/app/pages/frontend/components/newsletter/newsletter.ts
+++ src/app/pages/frontend/components/newsletter/newsletter.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-newsletter',
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+     MatIconModule
+  ],
   templateUrl: './newsletter.html',
   styleUrl: './newsletter.css',
 })
